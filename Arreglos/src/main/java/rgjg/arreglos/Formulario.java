92adc0bafc4487b2f2e3ca5fff80482e5ea9759c
Roberto Jimenez <robertojimenezg@gmail.com>
2023-03-22 13:42:33 -0600

    Componentes del formulario

--- Arreglos/src/main/java/rgjg/arreglos/Formulario.java
+++ Arreglos/src/main/java/rgjg/arreglos/Formulario.java
@@ -26,17 +26,45 @@ public class Formulario extends javax.swing.JFrame {
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
 
+    jLabel1 = new javax.swing.JLabel();
+    jTextField1 = new javax.swing.JTextField();
+    jButton1 = new javax.swing.JButton();
+
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+    jLabel1.setText("Nombre:");
+
+    jTextField1.setName("txtNombre"); // NOI18N
+
+    jButton1.setLabel("Guardar");
+    jButton1.setName("btnGuardar"); // NOI18N
 
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
     layout.setHorizontalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 400, Short.MAX_VALUE)
+      .addGroup(layout.createSequentialGroup()
+        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+          .addGroup(layout.createSequentialGroup()
+            .addGap(29, 29, 29)
+            .addComponent(jLabel1)
+            .addGap(28, 28, 28)
+            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+          .addGroup(layout.createSequentialGroup()
+            .addGap(69, 69, 69)
+            .addComponent(jButton1)))
+        .addContainerGap(229, Short.MAX_VALUE))
     );
     layout.setVerticalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 300, Short.MAX_VALUE)
+      .addGroup(layout.createSequentialGroup()
+        .addGap(59, 59, 59)
+        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+          .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+          .addComponent(jLabel1))
+        .addGap(49, 49, 49)
+        .addComponent(jButton1)
+        .addContainerGap(146, Short.MAX_VALUE))
     );
 
     pack();
@@ -78,5 +106,8 @@ public class Formulario extends javax.swing.JFrame {
   }
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
+  private javax.swing.JButton jButton1;
+  private javax.swing.JLabel jLabel1;
+  private javax.swing.JTextField jTextField1;
   // End of variables declaration//GEN-END:variables
 }
