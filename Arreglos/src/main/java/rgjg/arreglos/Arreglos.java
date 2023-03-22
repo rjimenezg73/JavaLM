3de91570a2d504cc238d0b23845b4e53c2432a17
Roberto Jimenez <robertojimenezg@gmail.com>
2023-03-22 13:35:57 -0600

    Segunda Parte del Curso - Arreglos

---
+++ Arreglos/src/main/java/rgjg/arreglos/Arreglos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package rgjg.arreglos;
+
+/**
+ *
+ * @author macbook
+ */
+public class Arreglos {
+
+    public static void main(String[] args) {
+      Formulario ventana = new Formulario();
+      
+      System.out.println("Hello World!");
+      ventana.show();
+    }
+}
